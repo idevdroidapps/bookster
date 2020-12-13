f4ec038ad572aeded3187262523d7de787b54b0a
James Campbell <james.campbell@shield.ai>
2020-12-13 06:45:21 -0500

    models

--- app/src/main/java/com/idevdroidapps/bookster/data/models/ImageLinks.kt
+++ app/src/main/java/com/idevdroidapps/bookster/data/models/ImageLinks.kt
@@ -1,4 +1,13 @@
 package com.idevdroidapps.bookster.data.models
 
-class ImageLinks {
-}
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Immutable model class.
+ * Objects of this type are received from the GoogleBooks API, therefore all the fields are annotated
+ * with the serialized name.
+ */
+data class ImageLinks (
+    @field:SerializedName("smallThumbnail") val smallThumbnail: String,
+    @field:SerializedName("thumbnail") val thumbnail: String,
+)
--- app/src/main/java/com/idevdroidapps/bookster/data/models/VolumeInfo.kt
+++ app/src/main/java/com/idevdroidapps/bookster/data/models/VolumeInfo.kt
@@ -1,4 +1,18 @@
 package com.idevdroidapps.bookster.data.models
 
-class VolumeInfo {
-}
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Immutable model class.
+ * Objects of this type are received from the GoogleBooks API, therefore all the fields are annotated
+ * with the serialized name.
+ */
+data class VolumeInfo(
+    @field:SerializedName("title") val title: String,
+    @field:SerializedName("subtitle") val subtitle: String,
+    @field:SerializedName("authors") val authors: List<String>,
+    @field:SerializedName("publishedDate") val publishedDate: String,
+    @field:SerializedName("description") val description: String,
+    @field:SerializedName("averageRating") val averageRating: String,
+    @field:SerializedName("imageLinks") val imageLinks: ImageLinks
+)
